3371b2c8277edf372a60799b1dc84f6133f2307b
David <dmed256@gmail.com>
2012-06-03 12:02:39 -0500

    main.cpp fixed

--- main.cpp
+++ main.cpp
@@ -10,6 +10,13 @@ int main(){
   device dev = o.getDevice(1,3);
   kernel k(&dev,"vectoradd.cl");
 
+  float* a = (float*) malloc(N*sizeof(float));
+  float* b = (float*) calloc(N*sizeof(float));
+  float* c = (float*) malloc(N*sizeof(float));
+
+  for(int i=0;i<N;i++)
+    a = i;
+
   ocl_mem cl_a = dev.malloc(N*sizeof(float),CL_MEM_READ_ONLY);
   ocl_mem cl_b = dev.malloc(N*sizeof(float),CL_MEM_READ_ONLY);
   ocl_mem cl_c = dev.malloc(N*sizeof(float),CL_MEM_WRITE_ONLY);
@@ -26,7 +33,6 @@ int main(){
   
   for(int i=0;i<N;i++)
     cout << c[i] << ',';
-
   cout << endl;
 
   free(a);
